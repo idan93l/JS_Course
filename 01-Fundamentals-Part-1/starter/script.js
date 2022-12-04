0517111e0457c8eac03ae54f52ed9ef023ecc61a
Idan Lev <idan.93lev@gmail.com>
2022-12-04 22:49:28 +0200

    01-fundamentals-part-1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,7 @@
+let h1 = document.querySelector("h1");
+
+if (h1.innerHTML === 'JavaScript Fundamentals – Part 1') console.log('holy moly!');
+
+h1.innerHTML = 'Changed Content!';
+
+h1.style.color = 'red';
