fc6dbfee0296459948a78bb5c8fc113c7f8610ee
Idan Lev <idan.93lev@gmail.com>
2022-12-06 15:39:46 +0200

    data types & declarations

---
+++ assignments/Data Types/script.js
@@ -0,0 +1,10 @@
+const country = 'Israel'
+const continent = 'Asia'
+const population = '8 Million'
+const isIsland = false;
+let language;
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
---
+++ assignments/Declarations/script.js
@@ -0,0 +1,14 @@
+const country = 'Israel'
+const continent = 'Asia'
+const isIsland = false;
+
+let population = '8 Million'
+let language;
+
+language = 'Hebrew';
+// continent = 'Europe';
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
